fea8da955fced486eb1a5aeb2b4ca2efbd98da24
Pierre-Luc Bacon <plbacon@cs.stanford.edu>
2019-09-17 15:44:38 -0700

    Explosing more to argparse

--- mdp/polytope_experiment.py
+++ mdp/polytope_experiment.py
@@ -98,46 +98,59 @@ def make_sep_objective(mdp, initial_distribution, temperature, temperature_logit
     return _objective
 
 
-def save_solution(params, temperature_transition, prefix='solution'):
+def save_solution(params, arguments, prefix='solution'):
     transition_logits, reward_hat = params
-    transition_hat = softmax((1./temperature_transition)*transition_logits)
+    transition_hat = softmax((1./arguments.temperature_transition)*transition_logits)
 
     solution = (onp.asarray(transition_hat), onp.asarray(reward_hat))
+    data = {'solution': solution, 'args': arguments}
+
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     filename = f"{prefix}-{timestamp}.pkl"
 
     with open(filename, 'wb') as file:
-        pickle.dump(solution, file)
+        pickle.dump(data, file)
 
 
-def implicit_differentiation(mdp, temperature, temperature_transition):
+def implicit_differentiation(mdp, arguments):
     nactions, nstates = mdp[0].shape[:2]
     true_transition, true_reward, true_discount = mdp
     initial_distribution = np.ones(nstates)/nstates
     expert_policy = np.array([[0.2, 0.8], [0.7, 0.3]])
 
-    # objective = jit(make_sep_objective(mdp, initial_distribution,
-    #                                   temperature, temperature_transition, expert_policy))
-    objective = jit(make_omd_objective(mdp, initial_distribution,
-                                       temperature, temperature_transition))
+    if arguments.structural is True:
+        objective = jit(make_sep_objective(mdp, initial_distribution,
+                                           arguments.temperature, arguments.temperature_transition,
+                                           expert_policy))
+    else:
+        objective = jit(make_omd_objective(mdp, initial_distribution,
+                                           arguments.temperature,
+                                           arguments.temperature_transition))
 
-    opt_init, opt_update, get_params = optimizers.adam(step_size=1e-2)
+    opt_init, opt_update, get_params = optimizers.adam(step_size=arguments.lr_performance)
 
     @jit
     def update(i, opt_state):
         params = get_params(opt_state)
         return opt_update(i, grad(objective)(params), opt_state)
 
+    @jit
+    def convergence_test(x_new, x_old):
+        return converge.max_diff_test(x_new, x_old, arguments.rtol, arguments.atol)
+
     init_params = (np.zeros_like(true_transition), np.zeros_like(true_reward))
     opt_state = opt_init(init_params)
 
-    for i in range(100):
-        opt_state = update(i, opt_state)
+    for i in range(arguments.max_iter):
         params = get_params(opt_state)
-        objective_value = objective(params)
-        print(objective_value)
+        print(i, objective(params))
+        opt_state = update(i, opt_state)
+        if convergence_test(get_params(opt_state), params):
+            break
 
-    save_solution(params, temperature_transition)
+    if arguments.discard is False:
+        prefix = f"{'sep' if arguments.structural else 'omd'}-implicit"
+        save_solution(params, arguments, prefix=prefix)
 
 
 def smooth_bellman_optimality_operator(x, params):
@@ -146,27 +159,34 @@ def smooth_bellman_optimality_operator(x, params):
                                          logsumexp((1. / temperature) * x, axis=1))
 
 
-def competitive_differentiation(mdp, temperature, temperature_transition):
+def competitive_differentiation(mdp, arguments):
     nstates = mdp[0].shape[-1]
     true_transition, true_reward, true_discount = mdp
     initial_distribution = np.ones(nstates)/nstates
+    expert_policy = np.array([[0.2, 0.8], [0.7, 0.3]])
+    expert_return = expected_return(mdp, initial_distribution, expert_policy)
 
     @jit
     def f(decision_variables):
         x, theta = decision_variables
         transition_logits, reward_hat = theta
 
-        transition_hat = softmax((1./temperature_transition)*transition_logits)
-        op_params = (transition_hat, reward_hat, true_discount, temperature)
+        transition_hat = softmax((1./arguments.temperature_transition)*transition_logits)
+        op_params = (transition_hat, reward_hat, true_discount, arguments.temperature)
 
         return smooth_bellman_optimality_operator(x, op_params)
 
     @jit
-    def objective(decision_variables):
+    def omd_objective(decision_variables):
         x, _ = decision_variables
-        policy = softmax((1./temperature)*x)
-        vf, _ = policy_evaluation(*mdp, policy)
-        return -(initial_distribution @ vf)
+        policy = softmax((1./arguments.temperature)*x)
+        return -expected_return(mdp, initial_distribution, policy)
+
+    @jit
+    def sep_objective(decision_variables):
+        x, _ = decision_variables
+        policy = softmax((1./arguments.temperature)*x)
+        return -expected_discounted_loglikelihood(mdp, initial_distribution, policy, expert_policy)
 
     @jit
     def equality_constraint(decision_variables):
@@ -174,18 +194,18 @@ def competitive_differentiation(mdp, temperature, temperature_transition):
         return f(decision_variables) - x
 
     # L((x, theta), lambda). Decision_variables are (x, theta)
+    objective = omd_objective
+    if arguments.structural is True:
+        objective = sep_objective
     init_mult, lagrangian, get_decision_variables = lagr_util.make_lagrangian(
         objective, equality_constraint)
 
-    step_size = 0.16
     opt_init, opt_update, get_lagrangian_variables = cga.cga_lagrange_min(
-        step_size, lagrangian, lr_multipliers=0.925)
-
-    rtol = atol = 1e-6
+        arguments.lr_performance, lagrangian, lr_multipliers=arguments.lr_constraint)
 
     @jit
     def convergence_test(x_new, x_old):
-        return converge.max_diff_test(x_new, x_old, rtol, atol)
+        return converge.max_diff_test(x_new, x_old, arguments.rtol, arguments.atol)
 
     @jit
     def step(i, opt_state):
@@ -202,15 +222,16 @@ def competitive_differentiation(mdp, temperature, temperature_transition):
     opt_state = opt_init(lagrangian_variables)
 
     omd_objective = jit(make_omd_objective(mdp, initial_distribution,
-                                           temperature, temperature_transition))
+                                           arguments.temperature, arguments.temperature_transition))
 
-    for i in range(1000):
+    for i in range(arguments.max_iter):
         old_lagrangian_variables = get_lagrangian_variables(opt_state)
         old_decision_variables = get_decision_variables(old_lagrangian_variables)
         print(
             i, objective(old_decision_variables),
             np.linalg.norm(equality_constraint(old_decision_variables)),
-            omd_objective(old_decision_variables[-1])
+            omd_objective(old_decision_variables[-1]),
+            expert_return
         )
         opt_state = step(i, opt_state)
         if convergence_test(get_lagrangian_variables(opt_state), old_lagrangian_variables):
@@ -218,19 +239,29 @@ def competitive_differentiation(mdp, temperature, temperature_transition):
 
     lagrangian_variables = get_lagrangian_variables(opt_state)
     decision_variables = get_decision_variables(lagrangian_variables)
-    save_solution(decision_variables[1], temperature_transition, prefix='competitive')
+    if arguments.discard is False:
+        prefix = f"{'sep' if arguments.structural else 'omd'}-competitive"
+        save_solution(decision_variables[1], arguments, prefix=prefix)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--implicit', action='store_true')
+    parser.add_argument('--structural', action='store_true')
     parser.add_argument('--temperature', type=float, default=1e-2)
     parser.add_argument('--temperature_transition', type=float, default=1e-2)
+    parser.add_argument('--lr_performance', type=float, default=0.15)
+    parser.add_argument('--lr_constraint', type=float, default=0.925)
+    parser.add_argument('--discard', action='store_true')
+    parser.add_argument('--rtol', type=float, default=1e-6)
+    parser.add_argument('--atol', type=float, default=1e-6)
+    parser.add_argument('--max_iter', type=int, default=1000)
+
     arguments = parser.parse_args()
 
     mdp = polytope.dadashi_fig2d()
     method = implicit_differentiation if arguments.implicit is True else competitive_differentiation
 
     t0 = time.time()
-    method(mdp, arguments.temperature, arguments.temperature_transition)
+    method(mdp, arguments)
     print('Time: ', time.time() - t0)
